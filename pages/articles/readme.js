c9ea2976fae01ae843c3847a52bb6b072c75fd88
Chanseok Lim <hi007chans@gmail.com>
2020-11-22 15:55:14 +0900

    add component/views and move, add Head tag

--- pages/articles/readme.js
+++ pages/articles/readme.js
@@ -1,9 +1,13 @@
 import Link from "next/link";
 import React from 'react';
+import Head from 'next/head';
 
 export default function ReadMe(props) {
     return (
         <div>
+            <Head>
+                <title>Read Me</title>
+            </Head>
             <h1>Read Me</h1>
             <Link href="/articles">
 
