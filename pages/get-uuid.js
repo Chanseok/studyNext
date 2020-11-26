830e48be7cd3950601c8dd94b5a66982c5f27948
Chanseok Lim <hi007chans@gmail.com>
2020-11-26 21:58:27 +0900

    추가: getInitialProps 사용예제

--- pages/get-uuid.js
+++ pages/get-uuid.js
@@ -1,7 +1,10 @@
 import Link from 'next/link';
 import React from 'react';
+import axios from 'axios';
 
 export default function GetUuid(props) {
+    console.log('GetUUid.render');
+    /*
     const [uuid, setUuid] = React.useState('')
     // "화면"에 마운트가 된 이후 동작; client side rendering
     React.useEffect(() => {
@@ -9,10 +12,13 @@ export default function GetUuid(props) {
             .then(response => response.json())
             .then(json => setUuid(json.uuid))
     }, [])
+    */
+    console.log(props.debug)
 
     return (
         <div className="h-screen v-full flex flex-col justify-center items-center">
-            {uuid};
+            <h1 className="mb-4">{props.label}</h1>
+            {props.uuid};
 
             <div className="mt-4">
                 <Link href="/">
@@ -21,5 +27,19 @@ export default function GetUuid(props) {
             </div>
         </div>
     )
+
+}
+
+// getInitialProps
+GetUuid.getInitialProps = async function(){
+    // 반드시 aync funciton으로 작성
+    
+    console.log('GetUuid.getInitialProps');
+    const response = await axios.get('http://localhost:3000'+'/api/uuid');
+    return {
+        label: 'UUID',
+        uuid: response.data.uuid,
+        debug: '디버그메시지: 데모 getInitialProps 사용법'
+    }
 
 }
