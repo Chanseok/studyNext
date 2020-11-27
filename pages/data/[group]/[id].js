01a6a395838bea3d6f664ade4ae2a4419ff509ed
Chanseok Lim <hi007chans@gmail.com>
2020-11-27 13:08:51 +0900

    추가: DynamicRoute 사용 예제 One More

---
+++ pages/data/[group]/[id].js
@@ -0,0 +1,13 @@
+export default function Page(props) {
+    return (
+        <div>
+            {props.path}
+        </div>
+    )
+}
+
+Page.getInitialProps = async function ( context ) {
+    return { 
+        path: `/data/${context.query.group}/${context.query.id}`,
+    }
+}
